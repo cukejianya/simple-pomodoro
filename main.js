dea0169b7a27b9402d94c235d39c8fa55c1e77b0
Chinedum Ukejianya <cukejianya@gmail.com>
2020-07-11 23:25:38 -0400

    make app a menubar app

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const { app, BrowserWindow, Menu, Tray } = require('electron');
+const { menubar } = require('menubar');
+const path = require('path');
 
-function createWindow() {
-  let win = new BrowserWindow({
+const mb = menubar({
+  index: path.join('file://', __dirname, 'dist/index.html'),
+  browserWindow: {
     width: 250,
     height: 350,
     titleBarStyle: 'hidden',
     resizable: false,
     maximizable: false,
-  });
-
-  win.loadFile('dist/index.html');
-}
-
-app.on('ready', createWindow)
+  },
+});
